22a4e716f57cfd897985cab236f199638824f511
CodingStudent2019 <tejalsaldanhaone@outlook.com>
2020-01-11 20:51:30 +0000

    globeconnections

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,7 @@
  var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
+          center: {lat: 39.952583, lng: -75.165222},
+          zoom: 10
         });
       }
