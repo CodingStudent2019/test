5b3b800edb161b2d04c6cc0012b3e44d2bc9d1d6
CodingStudent2019 <tejalsaldanhaone@outlook.com>
2020-01-12 15:20:31 +0000

    globeconnections

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,7 @@
-var defaultBounds = new google.maps.LatLngBounds(
-  new google.maps.LatLng(-33.8902, 151.1759),
-  new google.maps.LatLng(-33.8474, 151.2631));
-
-var input = document.getElementById('searchTextField');
-
-var searchBox = new google.maps.places.SearchBox(input, {
-  bounds: defaultBounds
-});
+ var googlemap;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 39.952583, lng: -75.165222},
+          zoom: 10
+        });
+      }
