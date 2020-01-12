cb41a01d6c5ea700fd2c4d16f21414b7a7dcee2d
CodingStudent2019 <tejalsaldanhaone@outlook.com>
2020-01-12 15:34:21 +0000

    globeconnections

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,20 @@
  var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 39.952583, lng: -75.165222},
-          zoom: 10
-        });
-      }
+
+function initMap() {
+ map = new google.maps.Map(document.getElementById('map'), {
+  center: {
+   lat: 39.952583,
+   lng: -75.165222
+  },
+  zoom: 10
+ });
+
+ var marker = new google.maps.Marker({
+  position: {
+   lat: 39.952583,
+   lng: -75.165222
+  },
+  map: map,
+  title: 'Milestone Project Two'
+ });
+}
