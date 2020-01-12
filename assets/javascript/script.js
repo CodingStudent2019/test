57e161c6cb35f6be66ad641bd3f934f2976f4485
CodingStudent2019 <tejalsaldanhaone@outlook.com>
2020-01-12 16:41:43 +0000

    globeconnections

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,7 +11,6 @@ function initMap() {
 
  marker = new google.maps.Marker({
     map: map,
-    icon: icon,
     draggable: true,
     animation: google.maps.Animation.DROP,
     position: {lat: 39.952583, lng: -75.165222}
@@ -19,10 +18,17 @@ function initMap() {
   marker.addListener('click', toggleBounce);
 }
 
-function toggleBounce() {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-  } else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
+function drop() {
+  for (var i =0; i < markerArray.length; i++) {
+    setTimeout(function() {
+      addMarkerMethod();
+    }, i * 600);
   }
+
+var input = document.getElementById('searchboxinput');
+
+var searchBox = new google.maps.places.SearchBox(input, {
+  bounds: defaultBounds
+});
+
 }
